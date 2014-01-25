820579016237d78a901f2b34c58c9e7d71055b00
unknown <supreme.overlord.of.lala.land@gmail.com>
2014-01-25 15:16:41 -0800

    temp stuff

---
+++ js/images.js
@@ -0,0 +1,23 @@
+images = [ // indexed by different levels
+	{ // LEVEL 1
+		// image resource URLs
+		character: {left:"../resources/images/char1.png", right:"../resources/images/char1.png", 
+					top:"../resources/images/char1.png", bottom:"../resources/images/char1.png"
+		},
+		npc: {left:"../resources/images/char2.png", right:"../resources/images/char2.png", 
+				top:"../resources/images/char2.png", bottom:"../resources/images/char2.png"
+		},
+		grenade: {left:"../resources/images/char_grenade.png", right:"../resources/images/char_grenade.png", 
+				top:"../resources/images/char_grenade.png", bottom:"../resources/images/char_grenade.png"
+		},
+	},
+	{ // LEVEL 2
+
+
+	},
+	// LEVEL 3
+	{
+
+
+	}
+]
