5445ccf2dcc04892cf08ea7c93617e1b37f3be89
Carolyn <d295806@drdrb.com>
2014-01-25 12:44:02 -0800

    NPC changes

--- js/npc.js
+++ js/npc.js
@@ -2,7 +2,8 @@ function Npc(x,y,a) {
 	// spawns an NPC at position x, y with aggression level a
 	this.x = x;
 	this.y = y;
-	this.
+	this.xdir = Engine.randInt(-1, 1);
+	this.ydir = Engine.randInt(-1, 1);
 	this.aggression = a;
 	this.alive = true;
 	this.update = function() {
