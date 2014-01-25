7f2749a73ec2fb3278fa01d946aba6a4ed2f819e
Brian Chau <me@brianchau.ca>
2014-01-25 12:57:19 -0800

    Added rudimentary key listener code

--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,27 @@
 // This file includes all the events..
 
-$('#world').keydown(function(event) {
-	console.log("Key pressed: ", event);
-})
+$(document).keydown(function(event) {
+	switch (event.keyCode) {
+		case 32:
+			console.log("Spacebar was pressed!");
+			break;
+		case 37:
+			console.log("Left was pressed!");
+			break;
+		case 38:
+			// Up
+			console.log("Up was pressed!");
+			break;
+		case 39:
+			// Right
+			console.log("Right was pressed!");
+			break;
+		case 40:
+			// Down
+			console.log("Down was pressed!");
+			break;
+		default:
+			console.log("Key pressed: ", event);
+			break;
+	}
+});
