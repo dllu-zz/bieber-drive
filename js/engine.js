42d95e1944a8d28dc1381b2a3a4eb78cb3db6398
Daniel Lu <dllu@dllu.net>
2014-01-26 12:37:47 -0800

    you can now undo in mapeditor; new colours for grenades

--- js/engine.js
+++ js/engine.js
@@ -12,7 +12,7 @@ var requestAnimFrame =
     function(callback, element) { setTimeout(callback, 1000/60); };
 
 $(document).ready(function() {
-    Engine.init()
+    Engine.init();
 });
 
 function Engine(){}
@@ -340,7 +340,7 @@ Engine.draw = function() {
         Engine.ctx.fillStyle = 'rgba(255,230,0,0.3)';
         Engine.ctx.fill();
     }
-    
+
     // draw goal
     Engine.ctx.beginPath();
     Engine.ctx.rect(Engine.goal.x-SPRITE_SIZE+2, Engine.goal.y-SPRITE_SIZE+2, 2*SPRITE_SIZE-4, 2*SPRITE_SIZE-4);
@@ -382,9 +382,9 @@ Engine.draw = function() {
         Engine.ctx.beginPath();
         Engine.ctx.arc(Engine.grenades[i].x, Engine.grenades[i].y, GRENADE_SIZE, 0, Math.PI*2, true);
         if(Engine.grenades[i].t%6<3) {
-            Engine.ctx.fillStyle = '#f00';
+            Engine.ctx.fillStyle = '#800';
         } else {
-            Engine.ctx.fillStyle = '#00f';
+            Engine.ctx.fillStyle = '#59f';
         }
         Engine.ctx.fill();
     }
